4793d3385b5a801f12dd308515b0540abcc9eb77
seyitalisoy <soyseyitali23@gmail.com>
2025-10-02 17:48:53 +0300

    Db Connection and Link Class

---
+++ backend/Entities/Concrete/Link.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
