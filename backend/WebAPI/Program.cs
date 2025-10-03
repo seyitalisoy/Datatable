93029605f4a1d265c3b62be4ecf2393d0223d2b9
seyitalisoy <soyseyitali23@gmail.com>
2025-10-03 22:03:21 +0300

    CORS applied

--- backend/WebAPI/Program.cs
+++ backend/WebAPI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -25,6 +27,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder =>
+    builder.WithOrigins("http://localhost:4200")
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowCredentials()
+);
 
 app.UseHttpsRedirection();
 
