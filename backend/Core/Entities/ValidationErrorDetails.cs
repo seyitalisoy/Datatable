57204b12e733fb982bb866693e15afded65fc396
seyitalisoy <soyseyitali23@gmail.com>
2025-10-13 14:13:31 +0300

    exception-handling

---
+++ backend/Core/Entities/ValidationErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        //fluent validation return type -> IEnumerable<ValidationFailure>
+        public IEnumerable<ValidationFailure> Errors { get; set; }
+    }
+}
---
+++ backend/Core/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder ConfigureCustomExceptionMiddleware(
+            this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
