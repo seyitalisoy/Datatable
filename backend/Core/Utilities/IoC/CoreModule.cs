1001181ff1c35e8ab30ec3404e84f001329453f6
seyitalisoy <soyseyitali23@gmail.com>
2025-10-13 12:09:21 +0300

    manually resolve services in AOP since Autofac handles DI instead of IServiceProvider

---
+++ backend/Core/Utilities/IoC/CoreModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.IoC
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            // servisler gelecek
+            services.AddSingleton<Stopwatch>();
+        }
+    }
+}
--- backend/WebAPI/Program.cs
+++ backend/WebAPI/Program.cs
@@ -1,6 +1,8 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Business.DependencyResolvers.Autofac;
+using Core.Extensions;
+using Core.Utilities.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDependencyResolvers(new CoreModule());
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
