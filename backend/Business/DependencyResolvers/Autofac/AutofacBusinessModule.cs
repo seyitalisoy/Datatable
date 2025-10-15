e5772031140dcba80da376659c3532a6c0cb5a43
seyitalisoy <soyseyitali23@gmail.com>
2025-10-15 09:52:34 +0300

    new Services added Autofac

--- backend/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ backend/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -9,6 +9,7 @@ using Business.Abstract;
 using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
+using Core.Utilities.Security.Jwt;
 using DataAccess.Abstract;
 using DataAccess.Concrete;
 
@@ -21,6 +22,13 @@ namespace Business.DependencyResolvers.Autofac
         {
             builder.RegisterType<LinkManager>().As<ILinkService>().SingleInstance();
             builder.RegisterType<EfLinkDal>().As<ILinkDal>().SingleInstance();
+
+            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
+
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
